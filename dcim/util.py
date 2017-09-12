595f4328730db541d7eb4c83b3a6ec5c4956345e
Eric Appelt <eric.appelt@gmail.com>
2017-09-12 16:31:47 -0500

    Add draw_rack util function and tests

--- dcim/util.py
+++ dcim/util.py
@@ -22,3 +22,67 @@ def expand_brackets(input_string):
     suffix = input_string.split(']')[-1]
 
     return ['{}{}{}'.format(prefix, x, suffix) for x in range(start, end+1)]
+
+
+def draw_rack(
+    height, width=64,
+    labels=(), positions=(), heights=(),
+    display=True
+):
+    """
+    Create an ASCII-art representation of a server rack given a list of
+    device labels, positions, and heights, and optionally print it
+    to standard output.
+
+    :param int height: The height of the rack (U)
+    :param int width: Display width to reserve for labels
+    :param list(str) labels: Names of the devices in the rack
+    :param list(int) positions: Positions of the devices in the rack
+    :param list(int) heights: Heights of the devices in the rack
+    :param bool display: print the result if True
+
+    :returns: ASCII-art representation of the rack as a list of strings,
+        one per line.
+    :rtype: list(str)
+    """
+    devices = list(zip(positions, heights, labels))
+    devices.sort(reverse=True)
+    devices.insert(0, None)
+
+    spacer = '+----+' + '-'*width + '+'
+    emptyslot = '|    |' + ' '*width + '|'
+    
+    drawing = []
+    u = 1
+    device_top = True
+    current_device = devices.pop()
+
+    while (u <= height):
+        if current_device and current_device[0] == u:
+            drawing.append(spacer)
+            label = current_device[2][:width-3].ljust(width-3)
+            drawing.append('|U{:03}|| {}||'.format(u, label))
+            u += 1
+            for _ in range(current_device[1] - 1):
+                drawing.append('|    ||' + ' '*(width-2) + '||')
+                drawing.append('|U{:03}||{}||'.format(u, ' '*(width-2)))
+                u += 1
+            current_device = devices.pop()
+            device_top = True
+        else:
+            if device_top:
+                drawing.append(spacer)
+            else:
+                drawing.append(emptyslot)
+            drawing.append('|U{:03}|{}|'.format(u, ' '*width))
+            device_top = False
+            u += 1
+
+    drawing.append(spacer)
+    drawing.reverse()
+
+    if display:
+        for line in drawing:
+            print(line)
+
+    return drawing
--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,7 +1,12 @@
 """
 Tests for the utils module
 """
-from dcim.util import expand_brackets
+from textwrap import dedent
+
+from dcim.util import (
+    expand_brackets,
+    draw_rack
+)
 
 
 class TestBracketExpand:
@@ -22,3 +27,94 @@ class TestBracketExpand:
 
     def test_null_range(self):
         assert expand_brackets('foo[9-7]') == []
+
+
+class TestDrawRack:
+    """
+    Tests for the draw_rack function
+    """
+    def test_empty_rack(self):
+        expected = """
+            +----+----------------------------------------+
+            |U008|                                        |
+            |    |                                        |
+            |U007|                                        |
+            |    |                                        |
+            |U006|                                        |
+            |    |                                        |
+            |U005|                                        |
+            |    |                                        |
+            |U004|                                        |
+            |    |                                        |
+            |U003|                                        |
+            |    |                                        |
+            |U002|                                        |
+            |    |                                        |
+            |U001|                                        |
+            +----+----------------------------------------+
+        """
+        expected = dedent(expected).strip()
+
+        result = draw_rack(8, width=40, display=False)
+
+        assert expected == '\n'.join(result)
+
+    def test_non_adjacent_devices(self):
+        expected = """
+            +----+----------------------------------------+
+            |U008|                                        |
+            +----+----------------------------------------+
+            |U007||                                      ||
+            |    ||                                      ||
+            |U006||                                      ||
+            |    ||                                      ||
+            |U005|| bar                                  ||
+            +----+----------------------------------------+
+            |U004|                                        |
+            |    |                                        |
+            |U003|                                        |
+            +----+----------------------------------------+
+            |U002|| foo                                  ||
+            +----+----------------------------------------+
+            |U001|                                        |
+            +----+----------------------------------------+
+        """
+        expected = dedent(expected).strip()
+
+        result = draw_rack(8, width=40, display=False,
+            labels=['foo', 'bar'],
+            positions=[2, 5],
+            heights=[1, 3]
+        )
+
+        assert expected == '\n'.join(result)
+
+    def test_adjacent_devices(self):
+        expected = """
+            +----+----------------------------------------+
+            |U008||                                      ||
+            |    ||                                      ||
+            |U007||                                      ||
+            |    ||                                      ||
+            |U006||                                      ||
+            |    ||                                      ||
+            |U005|| baz                                  ||
+            +----+----------------------------------------+
+            |U004||                                      ||
+            |    ||                                      ||
+            |U003||                                      ||
+            |    ||                                      ||
+            |U002|| bar                                  ||
+            +----+----------------------------------------+
+            |U001|| foo                                  ||
+            +----+----------------------------------------+
+        """
+        expected = dedent(expected).strip()
+
+        result = draw_rack(8, width=40, display=False,
+            labels=['foo', 'bar', 'baz'],
+            positions=[1, 2, 5],
+            heights=[1, 3, 4]
+        )
+
+        assert expected == '\n'.join(result)
