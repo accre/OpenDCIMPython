11fe73e96563532d2d13c4137a6a01a5c27fb654
Eric Appelt <eric.appelt@gmail.com>
2017-09-16 15:01:46 -0500

    Add unit testing for the client using a fake cache
    Add some unit tests for the existing client functions
    by exploiting the new cache function and creating a
    client with a pre-populated cache of OpenDCIM server
    responses for a mock-datacenter. Note that these tests
    are dependent on the OpenDCIM API not changing!

---
+++ tests/unit/test_client.py
@@ -0,0 +1,77 @@
+"""
+Tests for the dcim.DCIMClient class that can be performed without
+interacting with an OpenDCIM server. This can be accomplished by
+pre-populating the cache with some device, cabinet, and datacenter
+requests.
+"""
+import pytest
+from textwrap import dedent
+
+from dcim.client import DCIMClient
+
+from tests.assets.client_responses import populate_cache
+
+
+@pytest.fixture
+def client():
+    c = DCIMClient(caching=True)
+    c.cache = populate_cache()
+    return c
+
+
+class TestClientLocate:
+    """
+    Tests for the DCIMClient.locate method
+    """
+    def test_no_parents(self, client):
+        expected = {
+            'datacenter': 'Foo 101',
+            'cabinet': 'A01',
+            'position': 1,
+            'parent_devices': []
+        }
+        assert client.locate('node101') == expected
+
+    def test_parents(self, client):
+        expected = {
+            'datacenter': 'Foo 101',
+            'cabinet': 'A01',
+            'position': 4,
+            'parent_devices': ['chassisA']
+        }
+        assert client.locate('node103') == expected
+
+
+class TestClientShowrack:
+    """
+    Tests for the client showrack function
+    """
+    def test_showrack(self, client):
+        expected = """\
+            +----+--------------------------------+
+            |U010|                                |
+            |    |                                |
+            |U009|                                |
+            |    |                                |
+            |U008|                                |
+            |    |                                |
+            |U007|                                |
+            |    |                                |
+            |U006|                                |
+            +----+--------------------------------+
+            |U005||                              ||
+            |    ||                              ||
+            |U004|| chassisA (node104, node103)  ||
+            +----+--------------------------------+
+            |U003|                                |
+            +----+--------------------------------+
+            |U002|| node102                      ||
+            +----+--------------------------------+
+            |U001|| node101                      ||
+            +----+--------------------------------+
+        """
+        expected = dedent(expected).strip()
+
+        result = '\n'.join(client.showrack('A01', width=40))
+
+        assert result == expected
